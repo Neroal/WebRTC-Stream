efea1ba4eda70fff2bf07ece63e48fd26bc96f0c
Neroal Wu <neroal.Wu@north.com.tw>
2024-01-12 15:38:37 +0800

    first commit

---
+++ public/javascripts/srs.sdk.js
@@ -0,0 +1,57 @@
+'use strict';
+
+class SrsRtcWhipWhepAsync {
+
+    constructor() {
+        this.constranits = {
+            audio: true,
+            video: {
+                width: { ideal: 320, max: 576 }
+            }
+        }
+
+        this.pc = new RTCPeerConnection(null);
+        this.stream = new MediaStream();
+
+        this.pc.ontrack = (event) => {
+            this.ontrack && this.ontrack(event);
+        }
+    }
+
+    async play(url) {
+        if (url.indexOf('/whip-play/') === -1 && url.indexOf('/whep/') === -1) throw new Error(`invalid WHEP url ${url}`);
+
+        this.pc.addTransceiver("audio", { direction: "recvonly" });
+        this.pc.addTransceiver("video", { direction: "recvonly" });
+
+        const offer = await this.pc.createOffer();
+        await this.pc.setLocalDescription(offer)
+
+        const answer = await new Promise((resolve, reject) => {
+            console.log('Generated Offer:', offer);
+
+            const xhr = new XMLHttpRequest();
+
+            xhr.onload = () => {
+                if (xhr.readyState !== xhr.DONE) return;
+                if (xhr.status !== 200 && xhr.status !== 201) return reject(xhr);
+
+                const data = xhr.responseText;
+                console.log('Get answer:', data);
+                return data.code ? reject(xhr) : resolve(data);
+            }
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/sdp');
+            xhr.send(offer.sdp);
+        })
+
+        this.pc.setRemoteDescription(
+            new RTCSessionDescription({ type: 'answer', sdp: answer })
+        )
+    }
+
+    ontrack(event) {
+        this.stream.addTrack(event.track);
+    }
+}
+
---
+++ routes/player.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    const tableID  = tableParser(req.query);
+    res.render('player', { 
+        TITLE: 'CEBU WebRTC', 
+        TABLE_ID: tableID,
+    });
+});
+
+function tableParser(queryString) {
+    if (queryString && queryString.table) {
+        return queryString.table;
+    }
+
+    console.warn('Invalid table ID.', queryString);
+    return ''
+}
+
+module.exports = router;
